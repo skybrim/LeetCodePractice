ab30bb835f38450af890b4968bb3b834f8e02ff2
skybrim <throughskybrim@gmail.com>
2019-09-09 08:54:51 +0800

    翻转二叉树

--- LeetCodePractice/LeetCodePractice/ViewController.swift
+++ LeetCodePractice/LeetCodePractice/ViewController.swift
@@ -16,6 +16,33 @@ class ViewController: UIViewController {
         
         print(climbStairs_a(10))
     }
+    
+    @discardableResult
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        guard let root = root else {
+            return nil
+        }
+        invertTree(root.left)
+        invertTree(root.right)
+        swap(&root.left, &root.right)
+        return root
+    }
+    
+    
+    //Definition for a binary tree node.
+    public class TreeNode {
+        public var val: Int
+        public var left: TreeNode?
+        public var right: TreeNode?
+        public init(_ val: Int) {
+            self.val = val
+            self.left = nil
+            self.right = nil
+        }
+    }
+    
+    
+    
     //杨辉三角
     func generate(_ numRows: Int) -> [[Int]] {
         if numRows == 0 { return [] }
