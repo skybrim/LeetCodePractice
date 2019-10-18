253364573969e2264775d5024fc3ed1e9c9ad190
skybrim <throughskybrim@gmail.com>
2019-10-18 10:34:08 +0800

    初级算法

--- LeetCodePractice/LeetCodePractice/ViewController.swift
+++ LeetCodePractice/LeetCodePractice/ViewController.swift
@@ -8,26 +8,31 @@
 
 import UIKit
 
+
+class Solution {
+    
+    var originNums = [Int]()
+    
+    init(_ nums: [Int]) {
+        self.originNums = nums
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    func reset() -> [Int] {
+        return originNums
+    }
+    
+    /** Returns a random shuffling of the array. */
+    func shuffle() -> [Int] {
+        return originNums.shuffled()
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-//        var nums = [1, 0]
-//        moveZeroes(&nums)
-        
-//        print("abc")
-        print(isValidSudoku([
-          ["5","3",".",".","7",".",".",".","."],
-          ["6",".",".","1","9","5",".",".","."],
-          [".","9","8",".",".",".",".","6","."],
-          ["8",".",".",".","6",".",".",".","3"],
-          ["4",".",".","8",".","3",".",".","1"],
-          ["7",".",".",".","2",".",".",".","6"],
-          [".","6",".",".",".",".","2","8","."],
-          [".",".",".","4","1","9",".",".","5"],
-          [".",".",".",".","8",".",".","7","9"]
-        ]))
 
     }
     
